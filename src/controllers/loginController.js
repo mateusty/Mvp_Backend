1c2068b11d77b35cc81226586755cd475d75db44
slinky-snake <isabellebeariz07@gmail.com>
2026-09-09 19:55:52 -0300

    hash completo90%

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,10 +1,12 @@
 const authService = require("../services/authService");
 
 const doLogin = async (req, res) => {
-    const hash = await authService.gerarHash("5673"); 
+    const hash = await authService.gerarHash(req.body.password); 
     res.json(hash);
 }
 
 module.exports = {
     doLogin
 }
+
+/*node ./src/index.js*/
